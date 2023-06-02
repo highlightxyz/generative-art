c55ff8291851e02199d138efe080158ff52ed433
willdawson <will@highlight.xyz>
2023-06-02 11:19:43 -0400

    Add random functions for various use cases

--- hl-gen-art-v1.js
+++ hl-gen-art-v1.js
@@ -5,67 +5,97 @@
  * can be used within code based generative art.
  */
 
-let searchParams = new URLSearchParams(window.location.search)
+let searchParams = new URLSearchParams(window.location.search);
 
 const generateRandomHash = () => {
-    const alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-    return "0x" + Array(40).fill(0).map(_=>alphabet[(Math.random()*alphabet.length)|0]).join('');
-}
+  const alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+  return (
+    "0x" +
+    Array(40)
+      .fill(0)
+      .map((_) => alphabet[(Math.random() * alphabet.length) | 0])
+      .join("")
+  );
+};
+
+const generateRandomWalletAddress = () => {
+  const alphabet = "0123456789abcdef";
+  return (
+    "0x" +
+    Array(40)
+      .fill(0)
+      .map((_) => alphabet[(Math.random() * alphabet.length) | 0])
+      .join("")
+  );
+};
 
 function xmur3(str) {
-    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
-        h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
-            h = h << 13 | h >>> 19;
-    return function() {
-        h = Math.imul(h ^ h >>> 16, 2246822507),
-            h = Math.imul(h ^ h >>> 13, 3266489909);
-        return (h ^= h >>> 16) >>> 0;
-    }
+  for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+    (h = Math.imul(h ^ str.charCodeAt(i), 3432918353)),
+      (h = (h << 13) | (h >>> 19));
+  return function () {
+    (h = Math.imul(h ^ (h >>> 16), 2246822507)),
+      (h = Math.imul(h ^ (h >>> 13), 3266489909));
+    return (h ^= h >>> 16) >>> 0;
+  };
 }
 
 function sfc32New(a, b, c, d) {
-    return function() {
-        a |= 0; b |= 0; c |= 0; d |= 0;
-        var t = (a + b | 0) + d | 0;
-        d = d + 1 | 0;
-        a = b ^ b >>> 9;
-        b = c + (c << 3) | 0;
-        c = c << 21 | c >>> 11;
-        c = c + t | 0;
-        return (t >>> 0) / 4294967296;
-    }
+  return function () {
+    a |= 0;
+    b |= 0;
+    c |= 0;
+    d |= 0;
+    var t = (((a + b) | 0) + d) | 0;
+    d = (d + 1) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    c = (c + t) | 0;
+    return (t >>> 0) / 4294967296;
+  };
 }
 
 const seed = xmur3(searchParams.get("h") || generateRandomHash());
 
 const hl = {
-    tx: {
-        hash: searchParams.get("h") || generateRandomHash(),
-        blockHash: searchParams.get("bh"),
-        timestamp: searchParams.get("t"),
-        walletAddress: searchParams.get("wa"),
-        tokenId: searchParams.get("tid"),
+  tx: {
+    hash: searchParams.get("h") || generateRandomHash(),
+    blockHash: searchParams.get("bh") || generateRandomHash(),
+    timestamp: searchParams.get("t"),
+    walletAddress: searchParams.get("wa") || generateRandomWalletAddress(),
+    tokenId: searchParams.get("tid"),
+  },
+  random: sfc32New(seed(), seed(), seed(), seed()),
+  randomNum: (min, max) => {
+    return min + (max - min) * hl.random();
+  },
+  randomInt: (min, max) => {
+    return Math.floor(hl.randomNum(min, max + 1));
+  },
+  randomBool: (p) => {
+    return hl.random() < p;
+  },
+  randomElement: (array) => {
+    return array[hl.randomInt(0, array.length - 1)];
+  },
+  token: {
+    id: searchParams.get("tid"),
+    attributes: {},
+    capturePreview: function () {
+      window.dispatchEvent(new Event("CAPTURE_PREVIEW"));
+      setTimeout(() => this.capturePreview(), 500);
     },
-    rand: {
-        num: sfc32New(seed(), seed(), seed(), seed()),
+    setAttributes: function (attributes) {
+      this.attributes = attributes;
     },
-    token: {
-        id: searchParams.get("tid"),
-        attributes: {},
-        capturePreview: function () {
-            window.dispatchEvent(new Event("CAPTURE_PREVIEW"));
-            setTimeout(() => this.capturePreview(), 500)
-        },
-        setAttributes: function (attributes) {
-            this.attributes = attributes;
-        },
-        getAttributes: function () {
-            return this.attributes;
-        }
+    getAttributes: function () {
+      return this.attributes;
     },
-    context: {
-        previewMode: searchParams.get("pr") === "1"
-    }
+  },
+  context: {
+    previewMode: searchParams.get("pr") === "1",
+  },
 };
 
 window.$hl = hl;
