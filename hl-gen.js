55ae7debf7e6d445f02c92fc344c2adbd40bacae
Sarib Mahmood <sarib@highlight.xyz>
2024-06-11 15:38:18 -0400

    Update to add capture preview url

--- hl-gen.js
+++ hl-gen.js
@@ -1,6 +1,6 @@
 /**
  * Highlight Generative Art Script : V0
- * @version: 0.1.2
+ * @version: 0.1.3
  * @description The script exposes certain values and methods that
  * can be used within code based generative art.
  */
@@ -136,6 +136,10 @@ const hl = (function () {
         window.dispatchEvent(new Event("CAPTURE_PREVIEW"));
         setTimeout(() => this.capturePreview(), 500);
       },
+      capturePreviewUrl: function () {
+          window.dispatchEvent(new Event("CAPTURE_PREVIEW_URL"));
+          setTimeout(() => this.capturePreviewUrl(), 500);
+      },
       setTraits: function (traits) {
         this.traits = traits;
       },
@@ -161,7 +165,7 @@ const hl = (function () {
       scriptInfo: () => {
         return {
           name: "Highlight Generative Art Script",
-          version: "0.1.2",
+          version: "0.1.3",
           framework: "js",
         };
       },
